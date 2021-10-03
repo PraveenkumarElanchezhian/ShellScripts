bfad0ab044f792192577fba4e23098689bbe900c
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-10-03 20:29:44 +0530

    Shellprogram

--- empwagesproblem.sh
+++ empwagesproblem.sh
@@ -7,8 +7,14 @@ then
 working_hrs=8
 wage_hrs=20
 wage=$(($wage_hrs*$working_hrs))
+
+part_time_working_hrs=4
+wage_hrs=20
+part_time=$(($wage_hrs*$part_time_working_hrs))
+
 echo "If emp present"
 echo "Emp daily wage = $wage";
+echo "If emp working part time = $part_time wages";
 else
 echo "If else emp absent"
 fi
