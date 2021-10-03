91b6294218f4b4a45e12d5dc1d7636d4a74769fc
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-10-03 20:51:28 +0530

    shellprogram

--- empwagesproblem.sh
+++ empwagesproblem.sh
@@ -4,18 +4,33 @@ random=$(($RANDOM%2))
 echo $random
 if [ $random -eq 1 ]
 then
-working_hrs=8
-wage_hrs=20
-wage=$(($wage_hrs*$working_hrs))
-
-part_time_working_hrs=4
-wage_hrs=20
-part_time=$(($wage_hrs*$part_time_working_hrs))
-
 echo "If emp present"
-echo "Emp daily wage = $wage";
-echo "If emp working part time = $part_time wages";
 else
 echo "If else emp absent"
 fi
+
+case $random in
+1)
+        part_time_working_hrs=4
+        wage_hrs=20
+        part_time=$(($wage_hrs*$part_time_working_hrs*$random))
+        echo "If emp working part time = $part_time wages";
+
+        full_time_working_hrs=8
+        wage_hrs=20
+        full_time=$(($wage_hrs*$full_time_working_hrs*$random))
+	echo "If emp working full time = $full_time wages";
+	;;
+0)
+        part_time_working_hrs=4
+        wage_hrs=20
+        part_time=$(($wage_hrs*$part_time_working_hrs*$random))
+        echo "If emp working part time = $part_time wages";
+
+        full_time_working_hrs=8
+        wage_hrs=20
+        full_time=$(($wage_hrs*$full_time_working_hrs*$random))
+        echo "If emp working full time = $full_time wages";
+        ;;
+esac
 
